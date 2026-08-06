4e56deca4bc8d0ff33e33ae92b1e1525e17138a8
DoctorFinger <144325868+DoctorFinger@users.noreply.github.com>
2026-08-06 19:01:16 +0300

    Tehtävät 2.12.-2.15.

---
+++ osa2/puhelinluettelo/src/services/phoneService.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+
+const BASE_URL = "http://localhost:3001/persons"
+
+const getAll = () => 
+{
+    const request = axios.get(BASE_URL);
+    return request.then(response=>(response.data))
+}
+
+const create = (object) => 
+{
+    const request = axios.post(BASE_URL, object);
+    return request.then(response=>(response.data))
+}
+
+const update = (id, object) =>
+{
+    const url = `${BASE_URL}/${id}`
+    const request = axios.put(url, object);
+    return request.then(response=>(response.data))
+}
+
+const remove = (id) => 
+{
+    const url = `${BASE_URL}/${id}`
+    const request = axios.delete(url);
+    return request.then(response=>(response.data))
+}
+
+export default {
+    getAll,
+    create,
+    remove,
+    update
+}
