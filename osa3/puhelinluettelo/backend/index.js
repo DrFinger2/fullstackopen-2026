88042fb7efb799de877ad0a17550a8f33ba2760b
DoctorFinger <144325868+DoctorFinger@users.noreply.github.com>
2026-08-10 17:29:36 +0300

    maybe this works on render now?

--- osa3/puhelinluettelo/backend/index.js
+++ osa3/puhelinluettelo/backend/index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const dns = require('dns');
 
-dns.setServers(['1.1.1.1', '8.8.8.8'])
+// dns.setServers(['1.1.1.1', '8.8.8.8'])
 
 
 const generateId = require('./utils/generateId.js');
