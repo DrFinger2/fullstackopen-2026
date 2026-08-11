5c92c1600d3e5a1b2061c2378a77750ed0c20bf0
DoctorFinger <144325868+DoctorFinger@users.noreply.github.com>
2026-08-11 16:36:19 +0300

    testing if everything still works correctly locally.

--- osa3/puhelinluettelo/backend/index.js
+++ osa3/puhelinluettelo/backend/index.js
@@ -4,6 +4,12 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+
+// I needed to configure the DNS server to start the server locally due to my home computers internet configuration.
+// const dns = require('dns');
+// dns.setServers(['1.1.1.1', '8.8.8.8'])
+
+
 const { Person, connect, disconnect } = require('./models/person.js')
 const { formatName, formatNumber } = require('./utils/formatting.js');
 
