673d094b0237803b70d4ca23809d2744d99f61e7
DoctorFinger <144325868+DoctorFinger@users.noreply.github.com>
2026-08-15 18:52:23 +0300

    fixed a bug

--- osa5/blogilista/frontend/src/components/Blog.jsx
+++ osa5/blogilista/frontend/src/components/Blog.jsx
@@ -1,11 +1,12 @@
 import Header from './Header'
 
 const Blog = ({ blog }) => {
-  
-  <div className="blog-card">
-    <Header text={blog.title} href={blog.url} />
-    <strong>Author: </strong>{blog.author}
-  </div>
+  return (
+    <div className="blog-card">
+      <Header text={blog.title} href={blog.url} />
+      <strong>Author: </strong>{blog.author}
+    </div>
+  )
 }
 
 export default Blog
