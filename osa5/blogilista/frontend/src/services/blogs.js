8d8e42ef27a37467430f87cb9a46752bdf15f091
DoctorFinger <144325868+DoctorFinger@users.noreply.github.com>
2026-08-17 01:14:48 +0300

    great

--- osa5/blogilista/frontend/src/services/blogs.js
+++ osa5/blogilista/frontend/src/services/blogs.js
@@ -1,79 +1,58 @@
-import { useState, useEffect } from 'react'
-import { useParams, useNavigate } from 'react-router-dom'
-import Header from '../components/Header'
-import Loader from '../components/Loader'
-import blogService from '../services/blogs'
+import axios from 'axios'
 
-const BlogDetailsPage = ({ user }) => {
-  const { id } = useParams()
-  const navigate = useNavigate()
-  const [blog, setBlog] = useState(null)
-  const [loading, setLoading] = useState(true)
+const BASE_URL = '/api/blogs'
+let token = null
 
-
-  useEffect(() => {
-    const fetchBlog = async () => {
-      try {
-        const fetchedBlog = await blogService.get(id)
-        setBlog(fetchedBlog)
-      } catch (error) {
-        console.error('Error fetching blog:', error)
-      } finally {
-        setLoading(false)
-      }
-    }
-    fetchBlog()
-  }, [id])
-
-  if (loading) {
-    return (
-      <div className='blog-details-container'>
-        <Loader isLoading={loading} />
-      </div>
-    )
-  }
-
-  if (!blog) {
-    return (
-      <div className='blog-details-container'>
-        <p>Blog not found.</p>
-      </div>
-    )
-  }
-
-  const handleLike = async () => {
-    try {
-      const updatedBlog = await blogService.like(blog)
-      setBlog(updatedBlog)
-    } catch (error) {
-      console.error('Error liking blog:', error)
-    }
-  }
-
-  const handleRemove = async () => {
-    const title = `Remove blog "${blog.title}" by ${blog.author}?`
-    if (!window.confirm(title)) {
-      return
-    }
-    try {
-      await blogService.remove(blog.id)
-      navigate('/blogs')
-    } catch (error) {
-      console.error('Error removing blog:', error)
-    }
-  }
-
-  const isUserName = (blog?.user?.username === user?.username)
-  return (
-    <div className='blog-details-container'>
-      <Header text={blog.title}/>
-      <p><strong>URL: </strong><a href={blog.url} target='_blank' rel='noreferrer'>{blog.url}</a></p>
-      <p><strong>Author: </strong>{blog.author}</p>
-      <p> <strong>Likes: </strong>{blog.likes} <button onClick={handleLike}>Like</button> </p>
-      <p> <strong>Added by: </strong>{blog.user?.name || blog.user?.username || 'Unknown'} </p>
-      { isUserName && ( <button onClick={handleRemove}>Remove Blog</button> ) }
-    </div>
-  )
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`
 }
 
-export default BlogDetailsPage
+const getAll = async () => {
+  const response = await axios.get(BASE_URL)
+  return response.data
+}
+
+const create = async (newObject) => {
+  const config = {
+    headers: { Authorization:token }
+  }
+  const response = await axios.post(BASE_URL, newObject, config)
+  return response.data
+}
+
+const remove = async (blogId) => {
+  const config = {
+    headers: { Authorization: token }
+  }
+  const url = `${BASE_URL}/${blogId}`
+  const response = await axios.delete(url, config)
+  return response.data
+}
+
+const like = async (blog) => {
+  const config = {
+    headers: { Authorization: token }
+  }
+  const url = `${BASE_URL}/${blog.id}`
+  const updatedBlog = {
+    title: blog.title,
+    author: blog.author,
+    url: blog.url,
+    likes: blog.likes + 1
+  }
+  const response = await axios.put(url, updatedBlog, config)
+  return response.data
+}
+
+
+const update = async (blogId, newObject) => {
+  const config = {
+    headers: { Authorization:token }
+  }
+  const url = `${BASE_URL}/${blogId}`
+  const response = await axios.put(url, newObject, config)
+  return response.data
+}
+
+
+export default { getAll, create, remove, update, like, setToken }
