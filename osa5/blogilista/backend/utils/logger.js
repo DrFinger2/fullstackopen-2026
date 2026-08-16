edab4e0cadc83ff6193989f69847573fe09ff481
DoctorFinger <144325868+DoctorFinger@users.noreply.github.com>
2026-08-16 14:55:26 +0300

    log level

--- osa5/blogilista/backend/utils/logger.js
+++ osa5/blogilista/backend/utils/logger.js
@@ -1,15 +1,14 @@
-const { NODE_ENV } = process.env
+const { LOG_LEVEL } = process.env
+
+const LEVELS = { silent: 0, error: 1, info: 2 }
+const level = LEVELS[LOG_LEVEL || 'info']
 
 function info(...params) {
-    if (NODE_ENV !== 'test') {
-        console.log(...params)
-    }
+    if (level >= LEVELS.info) console.log(...params)
 }
 
 function error(...params){
-    if (NODE_ENV !== 'test') {
-        console.error(...params)
-    }
+    if (level >= LEVELS.error) console.error(...params)
 }
 
 module.exports = {
