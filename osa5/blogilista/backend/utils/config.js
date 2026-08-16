12f46ea89c8b1e8a213721c20015c5e66f6273b9
DoctorFinger <144325868+DoctorFinger@users.noreply.github.com>
2026-08-16 15:03:35 +0300

    little cleaner code

--- osa5/blogilista/backend/utils/config.js
+++ osa5/blogilista/backend/utils/config.js
@@ -4,8 +4,8 @@ const { NODE_ENV, PORT, EXTERNAL_URL, RENDER_EXTERNAL_URL } = process.env
 
 const MONGODB_URI = {
     test: process.env.MONGODB_URI_TEST,
-    development: process.env.MONGODB_URI_DEVELOPMENT,
-    production: process.env.MONGODB_URI_PRODUCTION
+    dev: process.env.MONGODB_URI_DEVELOPMENT,
+    prod: process.env.MONGODB_URI_PRODUCTION
 }[NODE_ENV] || process.env.MONGODB_URI_PRODUCTION
 
 module.exports = {
--- osa5/blogilista/backend/utils/logger.js
+++ osa5/blogilista/backend/utils/logger.js
@@ -1,7 +1,7 @@
 const { LOG_LEVEL } = process.env
 
 const LEVELS = { silent: 0, error: 1, info: 2 }
-const level = LEVELS[LOG_LEVEL || 'info']
+const level = LEVELS[LOG_LEVEL] ?? LEVELS.info
 
 function info(...params) {
     if (level >= LEVELS.info) console.log(...params)
