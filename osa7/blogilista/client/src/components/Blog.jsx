febf62bc413e7209f454038b4c896c1ce5671e83
DoctorFinger <144325868+DoctorFinger@users.noreply.github.com>
2026-08-21 02:48:53 +0300

    error boundary 2 test

--- osa7/blogilista/client/src/components/Blog.jsx
+++ osa7/blogilista/client/src/components/Blog.jsx
@@ -2,6 +2,8 @@ import { Link } from 'react-router-dom'
 import { Card, H3, Paragraph } from '../styles/Page.styles'
 
 function Blog({ blog }) {
+  throw new Error('Helloworld!')
+
   return (
     <Card data-testid="blog-card">
       <Link to={`/blogs/${blog.id}`}>
