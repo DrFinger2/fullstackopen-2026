09ac4d36fe277ffa616741f1b2cac98ba6b8dbd6
DoctorFinger <144325868+DoctorFinger@users.noreply.github.com>
2026-08-21 21:00:43 +0300

    6. Adding a book
    done and done

--- osa8/library-backend/index.js
+++ osa8/library-backend/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { v1: uuid } = require("uuid");
 
 let authors = [
   {
@@ -100,6 +101,7 @@ let books = [
 const typeDefs = /* GraphQL */ `
   type Author {
     name: String!
+    born: Int
     bookCount: Int!
   }
   type Book {
@@ -113,6 +115,14 @@ const typeDefs = /* GraphQL */ `
     authorCount: Int!
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
+  }
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book!
   }
 `;
 
@@ -146,8 +156,22 @@ const resolvers = {
       });
       return authors.map((author) => ({
         name: author.name,
+        born: author.born,
         bookCount: authorToCount[author.name] || 0,
       }));
+    },
+  },
+
+  Mutation: {
+    addBook: (parent, args) => {
+      const author = authors.find((a) => a.name === args.author);
+      if (!author) {
+        authors = authors.concat({ name: args.author, id: uuid() });
+      }
+
+      const book = { ...args, id: uuid() };
+      books = books.concat(book);
+      return book;
     },
   },
 };
