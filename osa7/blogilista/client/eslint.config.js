f901409a1c61070634b0cbac9f3c50c2db7a62f5
DoctorFinger <144325868+DoctorFinger@users.noreply.github.com>
2026-08-21 04:19:53 +0300

    eslint config to work with prettier

--- osa7/blogilista/client/eslint.config.js
+++ osa7/blogilista/client/eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default [
   { ignores: ['dist'] },
@@ -29,15 +30,9 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
-      indent: ['error', 2],
-      'linebreak-style': ['error', 'unix'],
-      quotes: ['error', 'single'],
-      semi: ['error', 'never'],
       eqeqeq: 'error',
-      'no-trailing-spaces': 'error',
-      'object-curly-spacing': ['error', 'always'],
-      'arrow-spacing': ['error', { before: true, after: true }],
       'no-console': 'off',
+      ...eslintConfigPrettier.rules,
     },
   },
 
--- osa7/blogilista/server/eslint.config.mjs
+++ osa7/blogilista/server/eslint.config.mjs
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import { defineConfig } from 'eslint/config'
 import stylisticJs from '@stylistic/eslint-plugin'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default defineConfig([
   {
@@ -25,15 +26,8 @@ export default defineConfig([
     },
     rules: {
       eqeqeq: 'error',
-      'no-trailing-spaces': 'error',
-      'object-curly-spacing': ['error', 'always'],
-      'arrow-spacing': ['error', { before: true, after: true }],
       'no-console': 'off',
-
-      '@stylistic/js/indent': ['error', 2],
-      '@stylistic/js/linebreak-style': ['error', 'unix'],
-      '@stylistic/js/quotes': ['error', 'single'],
-      '@stylistic/js/semi': ['error', 'never'],
+      ...eslintConfigPrettier.rules,
     },
   },
 ])
