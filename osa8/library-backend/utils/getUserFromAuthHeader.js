6a0d6f49d02912b6f4d6ca683afb8c3ac8093d41
DoctorFinger <144325868+DoctorFinger@users.noreply.github.com>
2026-08-23 03:57:26 +0300

    fix 3

--- osa8/library-backend/utils/getUserFromAuthHeader.js
+++ osa8/library-backend/utils/getUserFromAuthHeader.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/UserModel.js");
 
 const getUserFromAuthHeader = async (authHeader, secret) => {
-  const exists = authHeader && authHeader.startWith("Bearer ");
+  const exists = authHeader && authHeader.startsWith("Bearer ");
   if (exists) {
     const decoded = jwt.verify(authHeader.substring(7), secret);
     return await User.findById(decoded.id);
